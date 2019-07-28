9bb3e1bbe273b0bb322245ff5d3a7aa830f13e30
mghomedev <38830268+mghomedev@users.noreply.github.com>
2019-07-28 15:09:06 +0200

    introduce new state info.powerstatus to get the actual state from the TV using Sony Bravia JSON API when ping succeeds introduce new state triggerUpdateStatus to allow easy trigger of the power status refresh

--- main.js
+++ main.js
@@ -20,12 +20,13 @@ let device;
 let adapter;
 function startAdapter(options) {
     options = options || {};
+
+    
     Object.assign(options, {
         name: 'sony-bravia',
-        stateChange: function (id, state) {
+        stateChange: function (id, state) {            
             if (id && state && !state.ack) {
-                id = id.substring(id.lastIndexOf('.') + 1);
-                device.send(id);
+               handleStateCommand(id, state);
             }
         },
         ready: main
@@ -35,6 +36,22 @@ function startAdapter(options) {
 
     return adapter;
 }
+
+function handleStateCommand(id, state) {
+    adapter.log.debug("handleStateCommand called for id="+id);
+
+    if (id.endsWith("info.triggerUpdateStatus")) {    
+        if (state.val!=null && state.val) {                
+            setImmediate(() => {
+                checkStatus(); 
+            });
+        }
+    }else if (id.endsWith("info.powerstatus")) {
+        return;// ignored
+    }
+    id = id.substring(id.lastIndexOf('.') + 1);
+    device.send(id);
+} 
 
 function setConnected(_isConnected) {
     if (isConnected !== _isConnected) {
@@ -69,14 +86,63 @@ function main() {
     }
 
 }
-
 function checkStatus() {
-    ping.probe(adapter.config.ip, {log: adapter.log.debug}, function (err, result) {
+    ping.probe(adapter.config.ip, {
+        log: adapter.log.debug
+    }, function (err, result) {        
         if (err) {
             adapter.log.error(err);
         }
         if (result) {
             setConnected(result.alive);
+
+            if (device != null && result.alive) {
+                adapter.log.debug("device.getPowerStatus()...");
+                device.getPowerStatus().then(response => {                            
+                    adapter.log.debug("device.getPowerStatus: response=" + response);
+                    if (response != null) {
+                        if (response.status != null) {
+                            adapter.log.debug("device.getPowerStatus: status=" + response.status);
+                            adapter.setState('info.powerstatus', {
+                                val: response.status,
+                                ack: true
+                            });
+                        } else {
+                            adapter.setState('info.powerstatus', {
+                                val: "errorstatus",
+                                ack: true
+                            });
+                        }
+                        
+                        adapter.getState('info.triggerUpdateStatus', function(err, s) {
+                            if (s!=null && (s.val == null || s.val )) adapter.setState('info.triggerUpdateStatus', {val: false, ack: true}); 
+                        });
+
+                    } else {
+                        adapter.setState('info.powerstatus', {
+                            val: "errornoresponse",
+                            ack: true
+                        });
+                    }
+
+                }, reject => {
+                    adapter.log.debug("device.getPowerStatus failed ..." + reject);
+                    adapter.setState('info.powerstatus', {
+                        val: "errorcon",
+                        ack: true
+                    });
+                });
+            } else {
+                adapter.setState('info.powerstatus', {
+                    val: "errorping",
+                    ack: true
+                });
+            }
+        } else {
+            adapter.setState('info.powerstatus', {
+                val: "errorping",
+                ack: true
+            });
         }
     });
 }
